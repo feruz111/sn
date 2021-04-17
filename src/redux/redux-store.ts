b4faadfbd93cebff80e7aa63500894f007be7851
feruz111 <feruznazaroff111@gmail.com>
2021-04-18 00:02:21 +0100

    making login page

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,17 +4,19 @@ import { authReducer } from "./auth-reducer";
 import { dialogReducer } from "./dialogsReducer";
 import { profileReducer } from "./profileReducer";
 import { userReducer } from "./usersReducer";
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   messagesPage: dialogReducer,
   userPage: userReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer,
 });
 
-export type RootStoreType = ReturnType<typeof reducers>
+export type RootStoreType = ReturnType<typeof reducers>;
 
-let store = createStore(reducers,applyMiddleware(thunk));
+let store = createStore(reducers, applyMiddleware(thunk));
 
 // @ts-ignore
 window.store = store;
